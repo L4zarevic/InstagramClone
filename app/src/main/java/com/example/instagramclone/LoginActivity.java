07fde9de2492e9a6773e9cc0fa3ca87ebabd5efb
Nemanja Lazarević <lazarevic.nemanja96@gmail.com>
2020-03-10 19:39:40 +0100

    Logout

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.instagramclone;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -28,6 +29,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setTitle("Log In");
 
         //Initialization
         edtEmail = findViewById(R.id.edtEmail_LogInActivity);
@@ -71,8 +74,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             if (user != null && e == null) {
                                 FancyToast.makeText(LoginActivity.this, user.getUsername() + " is Logged in successfully ", Toast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
                                 transitionToSocialMediaActivity();
-                            } else {
-
                             }
                         }
                     });
@@ -87,7 +88,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     //A method that allows us to hide the keyboard
-    private void rootLoginLayout(View view) {
+    public void rootLoginLayout(View view) {
         try {
             InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
             inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
@@ -99,5 +100,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void transitionToSocialMediaActivity() {
         Intent intent = new Intent(LoginActivity.this, SocialMediaActivity.class);
         startActivity(intent);
+        finish();
     }
 }
