6469e59c8ac316179bf52845845f1eb6dc31b7e6
Nemanja Lazarević <lazarevic.nemanja96@gmail.com>
2020-03-11 19:45:07 +0100

    Showing Users Post,Users Info

--- app/src/main/java/com/example/instagramclone/UsersTab.java
+++ app/src/main/java/com/example/instagramclone/UsersTab.java
@@ -1,5 +1,6 @@
 package com.example.instagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,26 +8,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UsersTab extends Fragment {
+public class UsersTab extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ListView listView;
-    private ArrayList arrayList;
+    private ArrayList<String> arrayList;
     private ArrayAdapter arrayAdapter;
 
     public UsersTab() {
@@ -41,7 +49,10 @@ public class UsersTab extends Fragment {
         View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
         listView = view.findViewById(R.id.listView);
         arrayList = new ArrayList();
-        arrayAdapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,arrayList);
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setOnItemClickListener(UsersTab.this);
+        listView.setOnItemLongClickListener(UsersTab.this);
 
         //Displaying the message while the user list is being loadeddisplaying the message while the user list is being loaded
         final TextView txtLoadingUsers = view.findViewById(R.id.txtLoadingUsers);
@@ -65,5 +76,43 @@ public class UsersTab extends Fragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(getContext(), UsersPosts.class);
+        intent.putExtra("username", arrayList.get(position));
+        startActivity(intent);
+    }
+
+    //Method whick called when we log
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereEqualTo("username", arrayList.get(position));
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null && e == null) {
+                    //FancyToast.makeText(getContext(), user.get("profileProfession") + "", Toast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+
+                    final PrettyDialog prettyDialog = new PrettyDialog(getContext());
+                    prettyDialog.setTitle(user.getUsername() + "'s Info").setMessage(user.get("profileBio") + "\n"
+                            + user.get("profileProfession") + "\n"
+                            + user.get("profileHobbies") + "\n"
+                            + user.get("profileFavoriteSport")).setIcon(R.drawable.person).addButton("OK", //button text
+                            R.color.pdlg_color_white, //button text color
+                            R.color.pdlg_color_green, //button background color
+                            new PrettyDialogCallback() { //button OnClick listener
+                                @Override
+                                public void onClick() {
+                                    prettyDialog.dismiss();
+                                }
+                            }).show();
+                }
+            }
+        });
+        return true;
     }
 }
